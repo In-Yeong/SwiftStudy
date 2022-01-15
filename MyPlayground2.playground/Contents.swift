0fa48ab5631484bb7d7d858ddcf22eacda4e7e9f
In-Yeong <in0_2015@naver.com>
2022-01-15 14:32:57 +0900

    220115

--- MyPlayground2.playground/Contents.swift
+++ MyPlayground2.playground/Contents.swift
@@ -1,11 +1,38 @@
 import UIKit
 
+import Foundation
+
+struct MemoryAddress<T>: CustomStringConvertible {
+
+    let intValue: Int
+
+    var description: String {
+        let length = 2 + 2 * MemoryLayout<UnsafeRawPointer>.size
+        return String(format: "%0\(length)p", intValue)
+    }
+
+    // for structures
+    init(of structPointer: UnsafePointer<T>) {
+        intValue = Int(bitPattern: structPointer)
+    }
+}
+
+extension MemoryAddress where T: AnyObject {
+
+    // for classes
+    init(of classInstance: T) {
+        intValue = unsafeBitCast(classInstance, to: Int.self)
+        // or      Int(bitPattern: Unmanaged<T>.passUnretained(classInstance).toOpaque())
+    }
+}
+
 struct Sample {
     var mutableProperty: Int = 100
     let immutableProperty: Int = 100
     static var typeProperty: Int = 100
     
-    func instanceMethod() {
+    mutating func instanceMethod(_ item: Int) {
+        mutableProperty = item
         print("instance method")
     }
     
@@ -15,13 +42,18 @@ struct Sample {
 }
 
 var mutable: Sample = Sample()
+MemoryAddress(of: &mutable)
 mutable.mutableProperty = 200
+MemoryAddress(of: &mutable)
 // mutable.immutableProperty = 200
 // -> immutableProperty를 let으로 정의하였기 때문에 재정의 불가
 
 let immutable: Sample = Sample()
+
 // immutable.mutableProperty = 200
 // -> immutable을 let으로 정의하였기 때문에 그 내부도 재정의 불가
+// immutable.instanceMethod(200)
+
 
 Sample.typeProperty = 300
 Sample.typeMethod()
@@ -47,12 +79,16 @@ class ClassSample {
 }
 
 var mutableReference: ClassSample = ClassSample()
+MemoryAddress(of: mutableReference)
 mutableReference.mutableProperty = 200
+MemoryAddress(of: mutableReference)
 // mutableReference.immutableProperty = 200
 mutableReference.instanceMethod()
 
 let immutableReference: ClassSample = ClassSample()
+MemoryAddress(of: immutableReference)
 immutableReference.mutableProperty = 200
+MemoryAddress(of: immutableReference)
 // 클래스의 인스턴스는 참조 타입이므로 let으로 선언되었더라도 인스턴스 프로퍼티 값 변경 가능 < ????
 
 ClassSample.typeProperty = 200
